b28515862f429207258203d1c9c1eb3f55b04697
paoloricciuti <ricciutipaolo@gmail.com>
2023-09-04 16:45:52 +0200

    setup changeset

--- src/app.d.ts
+++ src/app.d.ts
@@ -7,6 +7,16 @@ declare global {
 		// interface PageData {}
 		// interface Platform {}
 	}
+	interface ViewTransition {
+        updateCallbackDone: Promise<void>;
+        ready: Promise<void>;
+        finished: Promise<void>;
+        skipTransition: () => void;
+    }
+
+    interface Document {
+        startViewTransition(updateCallback: () => (Promise<void>| void)): ViewTransition;
+    }
 }
 
 export {};

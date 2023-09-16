1973a28e7ed4a8c3bf977647c3a35e90fa48abd4
paoloricciuti <ricciutipaolo@gmail.com>
2023-09-16 16:17:41 +0200

    remove unused selector

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,15 +16,4 @@
 		color: theme(colors.slate.200);
 		height: 100vh;
 	}
-
-	.current::after {
-		content: '';
-		position: absolute;
-		z-index: -1;
-		background-color: theme(colors.slate.300);
-		inset: 0;
-		top: 100%;
-		height: 2px;
-		view-transition-name: underline;
-	}
 </style>

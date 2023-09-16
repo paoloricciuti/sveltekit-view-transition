34ec7124ee3a6a3dd91475d563d469236df8a576
paoloricciuti <ricciutipaolo@gmail.com>
2023-09-16 16:24:38 +0200

    Adapter static?

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import mdsvex_config from './mdsvex.config.js';
 import { mdsvex } from 'mdsvex';
